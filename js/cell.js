074d7ed72f9fb54fda3c8e75aa277d30533a1d50
Grzegorz Pach <maszeczuszets@protonmail.com>
2016-08-22 09:19:15 +0200

    created grid

---
+++ js/cell.js
@@ -0,0 +1,13 @@
+function Cell(x, y, size){
+    this.x = x;
+    this.y = y;
+    this.size = size;
+
+    this.draw_me = function(color){
+        fill('#fff');
+        if(color){
+            fill(color);
+        }
+        rect(this.x*this.size, this.y * this.size, this.size, this.size);
+    };
+}
--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
+var grid;
+var columns, rows;
+var cell_size = 10;
+
 function setup(){
     createCanvas(800, 600);
+
+    //columns and rows in grid
+    columns = floor(width/cell_size);
+    rows = floor(height/cell_size);
+
+    grid = new Array(columns);
+    for(var i = 0;i< columns; i++){
+        grid[i] = new Array(rows);
+    }
+    for(var i = 0;i<columns;i++){
+        for(var j = 0;j<rows;j++){
+            grid[i][j] = new Cell(i, j, cell_size);
+        }
+    }
 }
 function draw(){
     background('#212121');
+
+    for(var i = 0;i< columns;i++){
+        for(var j = 0;j < rows;j++){
+            grid[i][j].draw_me();
+        }
+
+    }
 }
