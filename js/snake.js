5d097948ca3eea2970c1c3db08df8ab580b8d227
Grzegorz Pach <maszeczuszets@protonmail.com>
2016-08-22 11:37:47 +0200

    when snake eats himself - game over

--- js/snake.js
+++ js/snake.js
@@ -22,19 +22,25 @@ function Snake() {
             this.snake_body.unshift(new Cell(accual_head.x + 1, accual_head.y, cell_size));
         }
         accual_head = this.snake_body[0];
-        if (accual_head.x >= columns || accual_head.x < 0 || accual_head.y >= rows || accual_head.y < 0) {
+        if (this.leaves_board() || this.eats_himself()) {
             setup();
         }
         if (accual_head.x == food.position.x && accual_head.y == food.position.y) {
-            console.log('eat!');
             this.make_longer();
             food.generate();
         }
-    }
+    };
+    this.leaves_board = function(){
+        var accual_head = this.snake_body[0];
+        if (accual_head.x >= columns || accual_head.x < 0 || accual_head.y >= rows || accual_head.y < 0) {
+            return true;
+        }
+        return false;
+    };
     this.make_longer = function () {
         var head = this.snake_body[0];
         this.snake_body.unshift(new Cell(head.x, head.y));
-    }
+    };
     this.change_direction_to = function (direction) {
         //when snake moves up - it can not move down
         //when snake moves down - it can not move up
@@ -43,11 +49,20 @@ function Snake() {
         if (!(direction == 0 && this.direction == 2) && !(direction == 2 && this.direction == 0) && !(direction == 1 && this.direction == 3) && !(direction == 3 && this.direction == 1)) {
             this.direction = direction;
         }
-    }
+    };
+    this.eats_himself = function(){
+        var head = this.snake_body[0];
+        for(var i = 1;i< this.snake_body.length; i++){
+            if(head.x == this.snake_body[i].x && head.y == this.snake_body[i].y){
+                return true;
+            }
+        }
+        return false;
+    };
     this.draw_me = function () {
         this.update();
         for (var i = 0; i < this.snake_body.length; i++) {
             this.snake_body[i].draw_me(this.snake_color);
         }
-    }
+    };
 }
